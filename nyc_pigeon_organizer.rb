db3fc8cbf04ebdadee4b77b0ffd76c2dc93cc8e1
Heloise Taillet <ht2394@columbia.edu>
2019-01-06 19:08:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,12 +20,13 @@ data.each do |attribute, list|
         array << check
         array << {attribute => [option.to_s]}
         new_hash[name] = array.flatten!
+        binding.pry
+
       end
 
       end
     end
   end
-  binding.pry
 
 end
 new_hash
