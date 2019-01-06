340c57727d966ef0a7bee8c8fc82622c2b3a2272
Heloise Taillet <ht2394@columbia.edu>
2019-01-06 18:59:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+new_hash = {}
+data.each do |attribute, list|
+  list.each do |option, names|
+    names.each do |name|
+      check = new_hash[name]
+      if check == nil
+        new_hash.store(name, attribute => option.to_s)
+      elsif check != nil
+        if check.keys[0] == attribute
+          array = []
+          array << check.values[0]
+          array << option.to_s
+          new_hash[name][attribute] = array
+        elsif check.keys[0] != attribute
+        array = []
+        array << check
+        array << {attribute => [option.to_s]}
+        new_hash[name] = array.flatten(1)
+        binding.pry
+      end
+      end
+    end
+  end
+end
+new_hash
 end
